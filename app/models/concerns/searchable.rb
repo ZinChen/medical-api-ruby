983814d258e1e44d1eccc4120a8bb51ff3d9e158
Yuri Zinchenko <zinchen777@gmail.com>
2020-06-25 15:06:12 +0300

    fix searchable typo

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -4,7 +4,7 @@ module Searchable
  module ClassMethods
   def search_by_name(query)
     starts_with_query = where('name ILIKE ?', "#{query}%")
-    contain_query = MedicalProcedure.where.not(id: starts_with_query.map(&:id)).where('name ILIKE ?', "%#{query}%")
+    contain_query = where.not(id: starts_with_query.map(&:id)).where('name ILIKE ?', "%#{query}%")
     starts_with_query + contain_query
   end
  end
